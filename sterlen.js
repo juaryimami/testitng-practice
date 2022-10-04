d3aa230de7fc76f85d37535e47d73bc5aa6a5207
juharyimami <juharyimami2011@gmail.com>
2022-10-04 18:40:04 +0300

    second commit

--- sterlen.js
+++ sterlen.js
@@ -1,4 +1,17 @@
 function stringLength(string) {
+  if(string.length<1 || string.length >10){
+    throw 'the string shoul be one or more and less than 10 characters!';
+  }
     return string.length;
   }
-  module.exports = stringLength;
+  function reverseString(string){
+    if(string.length<1 ){
+      throw 'the string shoul be one or more characters!';
+    }
+    let rev=[];
+    for(let i=0;i<string.length;i++){
+      rev[i]=string.charAt[string.length-i-1];
+    }
+    return rev;
+  }
+  module.exports = stringLength,reverseString;
--- sterlen.test.js
+++ sterlen.test.js
@@ -1,5 +1,9 @@
 const stringLength = require('./sterlen');
+const reverseString = require('./sterlen');
 
 test('count of the word "hello"', () => {
   expect(stringLength("hello")).toBe(5);
+});
+test('the revers of the string "book" is "koob" ', () => {
+  expect(reverseString("book")).toBe('koob');
 });
